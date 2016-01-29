9c19158d779486a837690139441205d2ee68fc05
Tom Lofts <dev@loftx.co.uk>
2016-01-29 10:24:03 -0500

    First release of Angular.js frontend for API

---
+++ www/js/app.js
@@ -0,0 +1,52 @@
+angular.module('gnucash', []).
+config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when('/accounts', {
+			templateUrl: 'partials/accounts/index.html',
+			controller: AccountListCtrl
+		}).
+		when('/accounts/:accountGuid', {
+			templateUrl: 'partials/accounts/detail.html',
+			controller: AccountDetailCtrl
+		}).
+		when('/customers', {
+			templateUrl: 'partials/customers/index.html',
+			controller: CustomerListCtrl
+		}).
+		when('/customers/:customerId', {
+			templateUrl: 'partials/customers/detail.html',
+			controller: CustomerDetailCtrl
+		}).
+		when('/vendors', {
+			templateUrl: 'partials/vendors/index.html',
+			controller: VendorListCtrl
+		}).
+		when('/vendors/:vendorId', {
+			templateUrl: 'partials/vendors/detail.html',
+			controller: VendorDetailCtrl
+		}).
+		when('/invoices', {
+			templateUrl: 'partials/invoices/index.html',
+			controller: InvoiceListCtrl
+		}).
+		when('/invoices/:invoiceId', {
+			templateUrl: 'partials/invoices/detail.html',
+			controller: InvoiceDetailCtrl
+		}).
+		when('/bills', {
+			templateUrl: 'partials/bills/index.html',
+			controller: BillListCtrl
+		}).
+		when('/bills/:billId', {
+			templateUrl: 'partials/bills/detail.html',
+			controller: BillDetailCtrl
+		}).
+		when('/reports', {
+			templateUrl: 'partials/reports/index.html'
+		}).
+		when('/reports/income-statement', {
+			templateUrl: 'partials/reports/income-statement.html',
+			controller: ReportIncomeStatementCtrl
+		}).
+		otherwise({redirectTo: '/accounts'});
+}]);
