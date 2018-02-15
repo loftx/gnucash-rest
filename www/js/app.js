2175761fa2b942d1e1efb17241e9814452d16b88
loftx <dev@loftx.co.uk>
2018-02-15 15:10:47 +0100

    Update to angular 1.4.3

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,5 @@
 angular.module('gnucash', [
+	'ngRoute',
 	'core',
 	'core.account', // this could be more modular https://docs.angularjs.org/tutorial/step_13 e.g. app/phone-detail/phone-detail.module.js / app/phone-detail/phone-detail.component.js:
 	'core.invoice',
