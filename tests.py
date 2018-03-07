bd77659c1d3ded378ec816b83aa71ccd050ac95e
loftx <dev@loftx.co.uk>
2018-03-07 20:31:33 +0100

    Additional account tests and minor fixes to pass tests

--- tests.py
+++ tests.py
@@ -2,22 +2,25 @@ import unittest
 import json
 import gnucash_rest
 import MySQLdb
+import warnings
 
-class Test(unittest.TestCase):
+class ApiTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = gnucash_rest.app.test_client()
         self.app.testing = True
 
+    def setup_database(self):
         database = MySQLdb.connect(host='localhost', user='root', passwd='oxford')
         cursor = database.cursor()
         sql = 'CREATE DATABASE test'
         cursor.execute(sql)
 
-    def tearDown(self):
+    def teardown_database(self):
+        warnings.filterwarnings('ignore', category = MySQLdb.Warning)
         database = MySQLdb.connect(host='localhost', user='root', passwd='oxford')
         cursor = database.cursor()
-        sql = 'DROP DATABASE test'
+        sql = 'DROP DATABASE IF EXISTS test'
         cursor.execute(sql)
 
     def get_error_type(self, method, url, data):
@@ -33,10 +36,14 @@ class Test(unittest.TestCase):
             return json_response['errors'][0]['type']
         else:
             raise ValueError('Non 400 error code: ' + response.status)
+ 
+class RootTestCase(ApiTestCase):
 
     def test_root(self):
         response = self.app.get('/')
         assert response.data == '"Gnucash REST API"'
+
+class SessionTestCase(ApiTestCase):
 
     def test_no_session(self):
         data = dict()
@@ -52,14 +59,14 @@ class Test(unittest.TestCase):
         )
         assert self.get_error_type('post', '/session', data) == 'InvalidIsNew'
 
-    def test_session_connection_isnew(self):
+    def test_session_isnew(self):
         data = dict(
             connection_string = 'mysql://root:oxford@localhost/test',
             is_new='1'
         )
         assert self.get_error_type('post', '/session', data) == 'InvalidIgnoreLock'
 
-    def test_session_connection(self):
+    def test_session(self):
         data = dict(
             connection_string = 'mysql://root:oxford@localhost/test',
             is_new = '1',
@@ -69,9 +76,122 @@ class Test(unittest.TestCase):
         # Logs
         # * 11:18:00  WARN <gnc.backend.dbi> [gnc_dbi_unlock()] No lock table in database, so not unlocking it.
 
+        # remove the database in case tests failed previously
+        self.teardown_database()
+
+        self.setup_database()
+
         # 201 CREATED
         response = self.app.post('/session', data=data)
         assert response.data == '"Session started"'
+
+        response = self.app.delete('/session')
+        assert response.data == '"Session ended"'
+
+        self.teardown_database()
+        
+
+class AccountsTestCase(ApiTestCase):
+
+    def test_accounts_no_session(self):
+        assert self.get_error_type('get', '/accounts', dict()) == 'SessionDoesNotExist'
+
+class AccountsSessionTestCase(ApiTestCase):
+
+    def setUp(self):
+        self.app = gnucash_rest.app.test_client()
+        self.app.testing = True
+
+        # remove the database in case tests failed previously
+        self.teardown_database()
+
+        self.setup_database()
+
+        data = dict(
+            connection_string = 'mysql://root:oxford@localhost/test',
+            is_new = '1',
+            ignore_lock = '0'
+        )
+
+        response = self.app.post('/session', data=data)
+        assert response.data == '"Session started"'
+
+    def tearDown(self):
+
+        response = self.app.delete('/session')
+        assert response.data == '"Session ended"'
+
+        self.teardown_database()
+
+    def test_accounts(self):
+
+        response = json.loads(self.app.get('/accounts').data)
+        assert response['name'] == 'Root Account'
+
+    # need tests for mangled add accounts
+
+    def test_add_top_account(self):
+
+        response = json.loads(self.app.post('/accounts', data=dict(
+            name = 'Test',
+            currency  = 'GBP',
+            account_type_id = '2'
+        )).data)
+
+        assert response['name'] == 'Test'
+
+    def test_add_account_no_data(self):
+
+        data = dict()
+
+        assert self.get_error_type('post', '/accounts', data) == 'NoAccountName'
+
+    def test_add_account_no_currency(self):
+        data = dict(
+            name = 'Test'
+        )
+        assert self.get_error_type('post', '/accounts', data) == 'InvalidAccountCurrency'
+
+    def test_add_account_invalid_currency(self):
+
+        data = dict(
+            name = 'Test',
+            currency  = 'XYZ'
+        )
+
+        #response = json.loads(self.app.post('/accounts', data=data).data)
+        #print response
+
+        assert self.get_error_type('post', '/accounts', data) == 'InvalidAccountCurrency'
+
+    def test_add_account_invalid_acount_type(self):
+
+        data = dict(
+            name = 'Test',
+            currency  = 'GBP',
+            account_type_id = 'X',
+        )
+
+        #response = json.loads(self.app.post('/accounts', data=data).data)
+        #print response
+
+        assert self.get_error_type('post', '/accounts', data) == 'InvalidAccountTypeID'
+
+    def test_add_account(self):
+
+        # this is test_accounts
+        response = json.loads(self.app.get('/accounts').data)
+        assert response['name'] == 'Root Account'
+
+        response = json.loads(self.app.post('/accounts', data=dict(
+            name = 'Test',
+            currency  = 'GBP',
+            account_type_id = '2',
+            parent_account_guid = response['guid'],
+        )).data)
+
+        assert response['name'] == 'Test'
+
 
 if __name__ == '__main__':
     unittest.main()
